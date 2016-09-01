b90150577c8f385a33baf394f896e004c779c8ff
Роман Степанов <r.stepanov@bars-open.ru>
2016-09-01 20:16:46 +0300

    RIMIS-1244 - изменение enum для rptm

--- deptree/updates/risar/__init__.py
+++ deptree/updates/risar/__init__.py
@@ -1,3 +1,3 @@
 # coding: utf-8
 
-from . import initial, radzinsky_risks, rimis_1076, rimis_1101, rimis_1241, rimis_1210
+from . import initial, radzinsky_risks, rimis_1076, rimis_1101, rimis_1241, rimis_1210, rimis_1244
---
+++ deptree/updates/risar/rimis_1244.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+from deptree. internals.base import DBToolBaseNode
+
+
+class RPTMetaModufyEnum(DBToolBaseNode):
+    name = 'rimis-1244'
+
+    @classmethod
+    def upgrade(cls):
+        with cls.connection as c:
+            c.execute(u'''
+ALTER TABLE rbRisarPrintTemplateMeta MODIFY type ENUM('Integer', 'Float', 'String', 'Boolean', 'Date', 'Time', 'List', 'Multilist', 'RefBook', 'RefBook.name', 'Organisation', 'OrgStructure', 'Person', 'Service', 'SpecialVariable', 'MKB', 'Area', 'MultiRefBook', 'MultiOrganisation', 'MultiOrgStructure', 'MultiPerson', 'MultiService', 'MultiMKB', 'MultiArea') NOT NULL;
+''')
