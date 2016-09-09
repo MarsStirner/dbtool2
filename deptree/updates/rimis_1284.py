641f9acd3abef65babf83d246747f6adb61945a4
Роман Степанов <r.stepanov@bars-open.ru>
2016-09-09 16:03:45 +0300

    RIMIS-1284 - длины полей в типах полисов

---
+++ deptree/updates/rimis_1284.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+from deptree. internals.base import DBToolBaseNode
+
+
+class RbPolicyTypeSerialAndNumberMaxLength(DBToolBaseNode):
+    name = 'rimis-1284'
+
+    @classmethod
+    def upgrade(cls):
+        with cls.connection as c:
+            c.execute(u'''
+ALTER TABLE `rbPolicyType`
+  ADD COLUMN `serial_maxlength` INT NULL AFTER `insurance_type`,
+  ADD COLUMN `number_maxlength` INT NULL AFTER `serial_maxlength`;
+''')
