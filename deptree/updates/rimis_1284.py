c440c61146b0e59dba86a34718109991db9e4406
Aleksandr Krivopustov <a.krivopustov@bars-open.ru>
2016-09-13 17:17:42 +0300

    RIMIS-1284 downgrade migration

--- deptree/updates/rimis_1284.py
+++ deptree/updates/rimis_1284.py
@@ -14,3 +14,11 @@ ALTER TABLE `rbPolicyType`
   ADD COLUMN `serial_maxlength` INT NULL AFTER `insurance_type`,
   ADD COLUMN `number_maxlength` INT NULL AFTER `serial_maxlength`;
 ''')
+    @classmethod
+    def downgrade(cls):
+        with cls.connection as c:
+            c.execute(u'''
+    ALTER TABLE `rbPolicyType`
+  DROP COLUMN `serial_maxlength`,
+  DROP COLUMN `number_maxlength`;
+    ''')
