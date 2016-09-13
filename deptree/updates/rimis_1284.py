9ec5d6bdb659b417728becfeaede2e50c935b37e
Aleksandr Krivopustov <a.krivopustov@bars-open.ru>
2016-09-13 18:14:46 +0300

    RIMIS-1284 update table

--- deptree/updates/rimis_1284.py
+++ deptree/updates/rimis_1284.py
@@ -7,12 +7,19 @@ class RbPolicyTypeSerialAndNumberMaxLength(DBToolBaseNode):
     name = 'rimis-1284'
 
     @classmethod
+    def not_actual_upgrade(cls):
+        with cls.connection as c:
+            c.execute(u'''
+                        ALTER TABLE `rbPolicyType`
+                        ADD COLUMN `serial_maxlength` INT NULL AFTER `insurance_type`,
+                        ADD COLUMN `number_maxlength` INT NULL AFTER `serial_maxlength`;
+                        ''')
+
+    @classmethod
     def upgrade(cls):
         with cls.connection as c:
             c.execute(u'''
-ALTER TABLE `rbPolicyType`
-  ADD COLUMN `serial_maxlength` INT NULL AFTER `insurance_type`,
-  ADD COLUMN `number_maxlength` INT NULL AFTER `serial_maxlength`;
+UPDATE hospital1_risar.rbPolicyType SET serial_mask = '?*?*?*?*', number_mask = '?9?9?9?9-?9?9?9?9-?9?9?9?9-?9?9?9?9'  WHERE cmiOld = 'cmiOld';
 ''')
     @classmethod
     def downgrade(cls):
