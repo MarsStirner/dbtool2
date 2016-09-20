9b4c0c591a5ad58c91b147beaaac48a5923391f3
Роман Степанов <r.stepanov@bars-open.ru>
2016-09-20 18:45:38 +0300

    RIMIS-1472 - Таблица для прикрепляемых к экшенам файлам

---
+++ deptree/updates/risar/rimis_1472.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+
+from deptree. internals.base import DBToolBaseNode
+
+
+class AddTableActionFileAttach(DBToolBaseNode):
+    name = 'rimis-1472'
+
+    @classmethod
+    def upgrade(cls):
+        with cls.connection as c:
+            c.execute(u'''
+CREATE TABLE `ActionFileAttach` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `action_id` int(11) NOT NULL COMMENT '{Action}',
+  `filemeta_id` int(11) NOT NULL COMMENT '{FileMeta}',
+  `setPerson_id` int(11) NOT NULL COMMENT 'Пользователь, прикрепивший файл {Person}',
+  `attachDate` datetime NOT NULL COMMENT 'Дата прикрепления',
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `fk_actionfileattach_action_idx` (`action_id`),
+  KEY `fk_actionfileattach_filemeta_idx` (`filemeta_id`),
+  KEY `fk_actionfileattach_setperson_idx` (`setPerson_id`),
+  CONSTRAINT `fk_actionfileattach_errand` FOREIGN KEY (`action_id`) REFERENCES `Action` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_actionfileattach_filemeta` FOREIGN KEY (`filemeta_id`) REFERENCES `FileMeta` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_actionfileattach_setperson` FOREIGN KEY (`setPerson_id`) REFERENCES `Person` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Относящиеся к action файлы';
+''')
