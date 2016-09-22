d4f422f9ce974f0d43af88705c18de8b16b78d57
Aleksandr Krivopustov <a.krivopustov@bars-open.ru>
2016-09-22 17:59:23 +0300

    RIMIS-1382  учета национальности пациента  | поле nationality_code в таблице Client
    Conflicts:
    
    	deptree/updates/__init__.py

---
+++ deptree/updates/rimis_1382.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from deptree.internals.base import DBToolBaseNode
+
+logger = logging.getLogger('dbtool')
+
+
+class Rimis1382(DBToolBaseNode):
+    name = 'rimis-1382'
+
+    @classmethod
+    def upgrade(cls):
+        with cls.connection as c:
+            logger.info(u"\t >>> Добавляем поле nationality_code в таблицу Client")
+            c.execute(u"""ALTER TABLE `Client`
+      ADD COLUMN `nationality_code` VARCHAR(255) NULL   COMMENT 'Национальность{rbOKIN_Nationality}' AFTER `sex`;""")
