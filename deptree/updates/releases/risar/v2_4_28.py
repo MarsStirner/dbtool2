b466af5a06bb7bdddaf2eb1d2e5bf68d2f7f31ac
Роман Степанов <r.stepanov@bars-open.ru>
2016-11-21 21:30:08 +0300

    RISAR release 2.4.28

---
+++ deptree/updates/releases/risar/v2_4_28.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+from deptree. internals.base import DBToolBaseNode
+
+
+class Risar_v2_4_28(DBToolBaseNode):
+    name = 'risar-v2.4.28'
+    depends = [
+        'risar-v2.4.24',
+        'rimis-1693', 'rimis-1717', 'rimis-1696', 'rimis-1711'
+    ]
