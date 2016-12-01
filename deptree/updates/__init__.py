77dfd4807f531660836af63b970bf80679feb1e3
Aleksandr Krivopustov <a.krivopustov@bars-open.ru>
2016-12-01 19:06:32 +0300

    RIMIS-785 добавил поле в rbRadzinskyRiskRate

--- deptree/updates/__init__.py
+++ deptree/updates/__init__.py
@@ -2,7 +2,7 @@
 from . import (
     zero_updates, rimis_1056, tmis_1022, tmis_1173, tmis_1179, tmis_1111, rimis_1248, tmis_1240,
     tmis_1250, tmis_1233, rimis_1099, rimis_1281, rimis_1382, rimis_1455, rimis_1348, rimis_1696,
-    tmis_1297, tmis_1230, tmis_1245, tmis_1224,
+    tmis_1297, tmis_1230, tmis_1245, tmis_1224, rimis_785,
     webmis_494,
     common,
     risar,
---
+++ deptree/updates/rimis_785.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from deptree.internals.base import DBToolBaseNode
+
+logger = logging.getLogger('dbtool')
+
+
+class RIMIS785_rbRadzinskyRiskRate(DBToolBaseNode):
+    name = 'rimis-785'
+
+    @classmethod
+    def upgrade(cls):
+        with cls.connection as c:
+            c.execute(u'''ALTER TABLE `rbRadzinskyRiskRate` ADD COLUMN `name_masc` VARCHAR(45) NULL  AFTER `value` ;''')
+            c.execute(u'''UPDATE rbRadzinskyRiskRate SET name_masc = 'низкий' where code='low';''')
+            c.execute(u'''UPDATE rbRadzinskyRiskRate SET name_masc = 'средний' where code='medium';''')
+            c.execute(u'''UPDATE rbRadzinskyRiskRate SET name_masc = 'высокий' where code='high';''')
+
