7759bcc79264a40b91ba6c71bb43d5b4d942b621
Dmitry Paschenko <paschenko@bars-open.ru>
2017-01-30 14:42:29 +0300

    RIMIS-2008 Тула. Расписание. Типы резерва, внеплановые записи

--- deptree/updates/risar/__init__.py
+++ deptree/updates/risar/__init__.py
@@ -2,4 +2,4 @@
 
 from . import (initial, radzinsky_risks, rimis_1076, rimis_1101, rimis_1241, rimis_1210, rimis_1244,
     rimis_1472, rimis_1465, rimis_1659, rimis_1717, rimis_1693, rimis_1711, rimis_1691, rimis_1797,
-    rimis_1820, rimis_1844, rimis_1852, rimis_1894, rimis_1854)
+    rimis_1820, rimis_1844, rimis_1852, rimis_1894, rimis_1854, rimis_2008)
