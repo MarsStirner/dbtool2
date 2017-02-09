b9988a2965f77ee3aa5befc2f58d88da62a8dc95
Роман Степанов <r.stepanov@bars-open.ru>
2017-02-09 21:18:36 +0300

    RIMIS-1696, RIMIS-1981 - перенос модулей

--- deptree/updates/__init__.py
+++ deptree/updates/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from . import (
     zero_updates, rimis_1056, tmis_1022, tmis_1173, tmis_1179, tmis_1111, rimis_1248, tmis_1240,
-    tmis_1250, tmis_1233, rimis_1099, rimis_1281, rimis_1382, rimis_1455, rimis_1348, rimis_1696,
+    tmis_1250, tmis_1233, rimis_1099, rimis_1281, rimis_1382, rimis_1455, rimis_1348,
     tmis_1297, tmis_1230, tmis_1245, tmis_1224, rimis_785, tmis_1160,
     webmis_494,
     common,
