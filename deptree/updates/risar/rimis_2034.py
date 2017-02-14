fac683794afc35404e7525c19f99382eb7948870
Oleg Tkachenko <o.tkachenko@bars-open.ru>
2017-02-14 17:07:20 +0300

    RIMIS-2034 увеличить длину поля

---
+++ deptree/updates/risar/rimis_2034.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+from deptree. internals.base import DBToolBaseNode
+
+
+class ExtendFieldLength(DBToolBaseNode):
+    name = 'rimis-2034'
+
+    @classmethod
+    def upgrade(cls):
+        with cls.connection as c:
+            c.execute(u'''
+                ALTER TABLE `rbProfMedHelp`
+                CHANGE COLUMN `name` `name` VARCHAR(256) NOT NULL COMMENT 'Наименование' AFTER `code`;
+            ''')
