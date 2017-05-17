d4b7755a20a0aa1a6698714bf1e0af461cd4b2d6
Роман Степанов <r.stepanov@bars-open.ru>
2017-05-17 13:28:14 +0300

    TMIS-1365 - Добавлена таблица для расширения списка шаблонов для контекста

--- deptree/updates/releases/gnc/v1_4_next.py
+++ deptree/updates/releases/gnc/v1_4_next.py
@@ -9,7 +9,6 @@ class MisGnc_v1_4_next(DBToolBaseNode):
     name = 'mis-v1.4.next'
     depends = [
         'mis-v1.4.35',
-        'tmis-1449',
-        'tmis-1459',
+        'tmis-1449', 'tmis-1459', 'tmis-1365'
 
     ]
---
+++ deptree/updates/tmis-1365.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from deptree.internals.base import DBToolBaseNode
+
+logger = logging.getLogger('dbtool')
+
+
+class PrintTemplateCETable(DBToolBaseNode):
+    name = 'tmis-1365'
+    depends = ['tmis-1365']
+
+    @classmethod
+    def upgrade(cls):
+        with cls.connection as c:
+            c.execute(u'''
+CREATE TABLE `rbPrintTemplateCE` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `context` varchar(64) NOT NULL COMMENT '{rbPrintTemplate.context} - родительский контекст печати, к которому будет добавляться шаблон',
+  `template_id` int(11) NOT NULL COMMENT '{rbPrintTemplate.id} - дополнительный шаблон печати (с контекстом free), который добавляется к выбранному контексту',
+  PRIMARY KEY (`id`),
+  KEY `fk_rbprinttemplatece_rbprinttemplate_idx` (`template_id`),
+  KEY `context_idx` (`context`),
+  CONSTRAINT `fk_rbprinttemplatece_rbprinttemplate` FOREIGN KEY (`template_id`) REFERENCES `rbPrintTemplate` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Список шаблонов печати (свободных), которые дополнительно добавляются к выбранному контексту';
+''')
+
